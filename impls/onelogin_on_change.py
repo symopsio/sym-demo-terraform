6e545ee25dcf93e7fb5c4f8caf57f63835808d31
Leslie Lam <leslie@symops.io>
2023-11-09 10:31:41 -0500

    update onelogin_on_change.py

--- impls/onelogin_on_change.py
+++ impls/onelogin_on_change.py
@@ -1,11 +1,11 @@
 def on_change_target_id(username, prompt_form):
-    customer_success_group = prompt_form.flow_vars["customer_success_target_id"]
+    customer_success_target_id = prompt_form.flow_vars["customer_success_target_id"]
 
     target_field = prompt_form.fields.get("target_id")
     customer_field = prompt_form.fields.get("customer")
 
-    # If the selected OneLogin group is the Customer Success group, then display the "customer" prompt field.
-    if target_field.value == customer_success_group:
+    # If the selected OneLogin role is the Customer Success role, then display the "customer" prompt field.
+    if target_field.value == customer_success_target_id:
         customer_field.visible = True
     else:
         customer_field.visible = False
